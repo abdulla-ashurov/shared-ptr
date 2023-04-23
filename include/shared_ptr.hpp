7d06f1529f05ecc1ed9d063409a25382fe0c0635
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-04-23 13:47:16 +0500

    Add prototype of shared_ptr class.

---
+++ include/shared_ptr.hpp
@@ -0,0 +1,32 @@
+#ifndef __UNIQUE_PTR_HPP__
+#define __UNIQUE_PTR_HPP__
+
+#include "aligned_storage.hpp"
+
+template <class T> 
+class shared_ptr {
+private:
+    size_t m_counter;
+    aligned_storage<T, sizeof(T)> m_object;
+
+public:
+    // constructors
+    shared_ptr();
+    shared_ptr(const T object);
+    shared_ptr(const shared_ptr<T> &other);
+
+    // operators
+    shared_ptr& operator=(const shared_ptr<T> &other);
+    T& operator*() const;
+    T* operator->() const;
+    operator bool() const;
+
+    // getters
+    T* get() const;
+    long use_count() const;
+
+    // destructor
+    ~shared_ptr();
+};
+
+#endif // __UNIQUE_PTR_HPP__
--- include/unique_ptr.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef __UNIQUE_PTR_HPP__
-#define __UNIQUE_PTR_HPP__
-
-#endif // __UNIQUE_PTR_HPP__
