1ec5fb025eb4ae8923703ee0b1bc4add26109b32
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-05-04 23:38:55 +0500

    Refactor alignment storage class.

--- include/aligned_storage.hpp
+++ include/aligned_storage.hpp
@@ -1,18 +1,18 @@
-#ifndef __ALIGNED_STORAGE_HPP__
-#define __ALIGNED_STORAGE_HPP__
+#ifndef __ALIGNED_STORAGE__
+#define __ALIGNED_STORAGE__
 
-template <class T, size_t Size>
-class aligned_storage {
+template <class T>
+class AlignedStorage {
 private:
-    uint8_t storage[Size * sizeof(T) + alignof(T)];
+    uint8_t storage[sizeof(T) + alignof(T)];
 
 public:
-    T* begin() {
-        uintptr_t padding = static_cast<uintptr_t>(static_cast<void*>(storage)) % alignof(T);
+    T *begin() {
+        uintptr_t padding = (uintptr_t)((void *)(storage)) % alignof(T);
         if (padding != 0)
-            return (T*)(storage + alignof(T) - padding);
+            return (T *)(storage + alignof(T) - padding);
 
-        return (T*)(storage);
+        return (T *)(storage);
     }
 };
 
